1466d7bab66202012bcfbadf191cb254588e3e70
oghuz <osmanjan.omar@gmail.com>
2017-03-12 16:58:12 -0700

    started writing core data stack

---
+++ Virtual Tourist/CoreDataStack.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStack.swift
+//  Virtual Tourist
+//
+//  Created by osmanjan omar on 3/12/17.
+//  Copyright © 2017 osmanjan omar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct CoreDataStack {
+    
+}
