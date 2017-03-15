e316644c4b66f39f1beed49e0e54fada229e6fe4
oghuz <osmanjan.omar@gmail.com>
2017-03-15 10:25:34 -0700

    implemented collection view data source and delegate methods

--- Virtual Tourist/CoreDataCollectionCollectionViewController.swift
+++ Virtual Tourist/CoreDataCollectionCollectionViewController.swift
@@ -38,6 +38,22 @@ class CoreDataCollectionCollectionViewController: UICollectionViewController {
     
 }
 
+extension CoreDataCollectionCollectionViewController {
+
+    //execute search
+    func executeSearch() {
+        if let fr = fetchResultsController {
+            do {
+                try fr.performFetch()
+            }
+            catch let error as NSError {
+                print("error while searching\(fetchResultsController), error: \(error)")
+            }
+        }
+    }
+
+}
+
 extension CoreDataCollectionCollectionViewController: NSFetchedResultsControllerDelegate {
     
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
@@ -73,74 +89,38 @@ extension CoreDataCollectionCollectionViewController: NSFetchedResultsController
             break
         }
     }
-    
-
 }
 
+
+// MARK: UICollectionViewDataSource
 extension CoreDataCollectionCollectionViewController {
     
-    //execute search
-    func executeSearch() {
-        if let fr = fetchResultsController {
-            do {
-                try fr.performFetch()
-            }
-            catch let error as NSError {
-                print("error while searching\(fetchResultsController), error: \(error)")
-            }
-        }
-    }
     
-
-    // MARK: UICollectionViewDataSource
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        if let fc = fetchResultsController {
+            return (fc.sections?.count)!
+        }
+        else {
+            return 0
+        }
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        if let fc = fetchResultsController {
+            return fc.sections![section].numberOfObjects
+        }
+        else{
+            return 0
+        }
     }
 
+    
+    // create collection view items in sub classes
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
+        fatalError("this method must be implemented in subclasse")
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
+   
 
 }
